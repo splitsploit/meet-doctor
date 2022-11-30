47e29a00201382da01239bc083e87cbfb639a6d5
unknown <bayuxbca@gmail.com>
2022-11-30 19:56:13 +0700

    commit slicing view

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+
+        @include('includes.frontsite.meta')
+        
+        <title>@yield('title') | MeetDoctor</title>
+
+        @stack('before-style')
+
+            @include('includes.frontsite.style')
+
+        @stack('after-style')
+
+    </head>
+    <body>
+
+        @include('components.frontsite.header')
+
+            @yield('content')
+
+        @include('components.frontsite.footer')
+
+        @stack('before-script')
+
+            @include('includes.frontsite.style')
+
+        @stack('after-script')
+    </body>
+</html>
--- resources/views/pages/frontsite/landing-page/index.blade.php
+++ resources/views/pages/frontsite/landing-page/index.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <p>got hii from <span style="color: salmon">Byyy</span></p>
-</body>
-</html>
+@extends('layouts.default')
+
+@section('title', 'Home')
+
+@section('content')
+    Home
+@endsection
