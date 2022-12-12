8342a90d7f9704fc5369b656b145dcf988384d3d
unknown <bayuxbca@gmail.com>
2022-12-12 20:08:42 +0700

    update assets & includes

--- resources/views/includes/backsite/style.blade.php
+++ resources/views/includes/backsite/style.blade.php
@@ -1,30 +1,26 @@
 <!-- BEGIN: Vendor CSS-->
-<link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/vendors.min.css">
-<link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/weather-icons/climacons.min.css">
-<link rel="stylesheet" type="text/css" href="../../../app-assets/fonts/meteocons/style.css">
-<link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/charts/morris.css">
-<link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/charts/chartist.css">
-<link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/charts/chartist-plugin-tooltip.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('/assets/backsite/app-assets/vendors/css/vendors.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/assets/backsite/app-assets/vendors/css/tables/datatable/datatables.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/assets/backsite/app-assets/vendors/css/tables/extensions/responsive.dataTables.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/assets/backsite/app-assets/vendors/css/forms/selects/select2.min.css') }}">
 <!-- END: Vendor CSS-->
 
 <!-- BEGIN: Theme CSS-->
-<link rel="stylesheet" type="text/css" href="../../../app-assets/css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="../../../app-assets/css/bootstrap-extended.css">
-<link rel="stylesheet" type="text/css" href="../../../app-assets/css/colors.css">
-<link rel="stylesheet" type="text/css" href="../../../app-assets/css/components.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/backsite/app-assets/css/bootstrap.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/backsite/app-assets/css/bootstrap-extended.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/backsite/app-assets/css/colors.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/backsite/app-assets/css/components.css') }}">
 <!-- END: Theme CSS-->
 
 <!-- BEGIN: Page CSS-->
-<link rel="stylesheet" type="text/css" href="../../../app-assets/css/core/menu/menu-types/vertical-menu.css">
-<link rel="stylesheet" type="text/css" href="../../../app-assets/css/core/colors/palette-gradient.css">
-<link rel="stylesheet" type="text/css" href="../../../app-assets/fonts/simple-line-icons/style.css">
-<link rel="stylesheet" type="text/css" href="../../../app-assets/css/pages/timeline.css">
-<link rel="stylesheet" type="text/css" href="../../../app-assets/css/pages/dashboard-ecommerce.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('/assets/backsite/app-assets/css/core/menu/menu-types/vertical-menu.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/assets/backsite/app-assets/css/core/colors/palette-gradient.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/assets/backsite/app-assets/css/core/colors/palette-tooltip.css') }}">
 <!-- END: Page CSS-->
 
 <!-- BEGIN: Custom CSS-->
-<link rel="stylesheet" type="text/css" href="../../../assets/css/style.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/backsite/assets/css/style.css') }}">
 <!-- END: Custom CSS-->
 
-{{-- Third Partd --}}
+{{-- Third Party --}}
 <link rel="stylesheet" href="{{ url('https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css') }}">
