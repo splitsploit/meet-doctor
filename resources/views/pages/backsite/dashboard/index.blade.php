a3da35430a7486268d944c07632de0f5ab6107ec
unknown <bayuxbca@gmail.com>
2022-12-13 10:28:47 +0700

    update footer

--- resources/views/pages/backsite/dashboard/index.blade.php
+++ resources/views/pages/backsite/dashboard/index.blade.php
@@ -4,6 +4,11 @@
 
 @section('content')
 
-    
+<div class="app-content content">
+    <div class="content-overlay"></div>
+    <div class="content-wrapper">
+        Home Dashboard
+    </div>
+</div>
 
 @endsection
